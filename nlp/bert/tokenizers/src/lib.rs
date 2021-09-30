ecb301b145d0995fab19ea70c1c7f4eb89031ae3
Mohit0928 <mohityadav0928@gmail.com>
2021-09-30 15:34:19 +0530

    code structring; Text output from text_extractor

--- nlp/bert/tokenizers/src/lib.rs
+++ nlp/bert/tokenizers/src/lib.rs
@@ -2,16 +2,16 @@
 pub mod tokenizer;
 pub mod vocab;
 
+use crate::alloc::string::{String, ToString};
 pub use crate::tokenizer::base_tokenizer::{
     ConsolidatableTokens, ConsolidatedTokenIterator, Mask, Offset, OffsetSize, Token,
     TokenIdsWithOffsets, TokenIdsWithSpecialTokens, TokenRef, TokenTrait, TokenizedInput,
     TokensWithOffsets,
 };
-use alloc::vec::Vec;
 use alloc::vec;
+use alloc::vec::Vec;
 use core::default::Default;
 use core::str::FromStr;
-use crate::alloc::string::{String, ToString};
 
 #[macro_use]
 extern crate alloc;
@@ -26,7 +26,7 @@ use hotg_rune_proc_blocks::{ProcBlock, Transform};
 #[derive(ProcBlock)]
 pub struct Tokenizers {
     bert_tokenizer: BertTokenizer,
-    bert_vocab: BertVocab
+    bert_vocab: BertVocab,
 }
 
 impl Default for Tokenizers {
@@ -37,55 +37,51 @@ impl Default for Tokenizers {
         let vocab_copy = vocab.clone();
         let bert_tokenizer = BertTokenizer::from_existing_vocab(vocab, true, true);
 
-        Tokenizers { bert_tokenizer:bert_tokenizer, bert_vocab: vocab_copy }
+        Tokenizers {
+            bert_tokenizer: bert_tokenizer,
+            bert_vocab: vocab_copy,
+        }
     }
 }
 
 impl Transform<(Tensor<u8>, Tensor<u8>)> for Tokenizers {
     type Output = (Tensor<i32>, Tensor<i32>, Tensor<i32>, Tensor<u8>);
 
-    fn transform (&mut self, s: (Tensor<u8>, Tensor<u8>)) -> (Tensor<i32>, Tensor<i32>, Tensor<i32>, Tensor<u8> ) {
+    fn transform(
+        &mut self,
+        s: (Tensor<u8>, Tensor<u8>),
+    ) -> (Tensor<i32>, Tensor<i32>, Tensor<i32>, Tensor<u8>) {
         let (s1, s2) = s;
         let underlying_bytes_1: &[u8] = s1.elements();
         let input_text_1: &str =
             core::str::from_utf8(underlying_bytes_1).expect("Input tensor should be valid UTF8");
+        assert!(!input_text_1.is_empty(), "Sentence 1 is empty");
 
         let underlying_bytes_2: &[u8] = s2.elements();
         let input_text_2: &str =
             core::str::from_utf8(underlying_bytes_2).expect("Input tensor should be valid UTF8");
+        assert!(!input_text_2.is_empty(), "Sentence 2 is empty");
 
         let tok: Tokenizers = Default::default();
-        // let token= tok.bert_tokenizer;
-        let vocab_copy = tok.bert_vocab;
-
 
         let TokenizedInput {
             mut token_ids,
             special_tokens_mask: _,
             mut segment_ids,
             ..
-        } = tok.bert_tokenizer.encode(input_text_1,
+        } = tok.bert_tokenizer.encode(
+            input_text_1,
             Some(input_text_2),
             384,
             &TruncationStrategy::LongestFirst,
             0,
         );
 
-        // let index = 0;
-        // for (i, item) in segment_ids.iter().enumerate() {
-        //     if item==1{
-        //         index = i;
-        //         break
-        //     }
-        // }
-
-        let ind = segment_ids.iter().position(|y| *y == 1);
-
-
-        let inde = match ind {
-        Some(index) => index as i64,
-        None => -1
-        };
+        let ind = segment_ids
+            .iter()
+            .position(|y| *y == 1)
+            .map(|index| index as i64)
+            .expect("Unable to find split between sentence 1 and sentence 2");
 
         let mut mask_ids: Vec<i32> = vec![1; token_ids.len()];
         token_ids.resize(384, 0);
@@ -93,27 +89,30 @@ impl Transform<(Tensor<u8>, Tensor<u8>)> for Tokenizers {
         segment_ids.resize(384, 0);
 
         let input_ids: Vec<i32> = token_ids.iter().map(|&x| x as i32).collect::<Vec<i32>>();
-      
+
         let seg_ids: Vec<i32> = segment_ids.iter().map(|&x| x as i32).collect::<Vec<i32>>();
 
         let mut words = String::new();
-        let tok_ids = &token_ids[inde as usize ..];
+        let tok_ids = &token_ids[ind as usize..];
+
         for id in tok_ids {
-            let s = BertVocab::id_to_token(&vocab_copy, &id);
+            let s = self.bert_vocab.id_to_token(*id);
 
             words.push_str(&s);
-            words.push_str("\n")
+            words.push_str("\n");
         }
-        words =words.to_string();
+        words = words.to_string();
         let words: Vec<u8> = words.as_bytes().to_vec();
 
         (
             Tensor::new_row_major(input_ids.into(), vec![1, 384]),
             Tensor::new_row_major(mask_ids.into(), vec![1, 384]),
             Tensor::new_row_major(seg_ids.into(), vec![1, 384]),
-            Tensor::new_vector(words)
+            Tensor::new_vector(words),
         )
     }
 }
 
 impl HasOutputs for Tokenizers {}
+
+
