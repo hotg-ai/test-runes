824b0995aeb6e98536a09ff0f0b4055bff8fa14d
mohit0928 <mohityadav0928@gmail.com>
2022-03-16 17:54:57 +0530

    added control logic runefile

---
+++ image/person-gesture/control_logic/src/lib.rs
@@ -0,0 +1,34 @@
+#![no_std]
+extern crate alloc;
+use alloc::vec::Vec;
+use hotg_rune_proc_blocks::{ProcBlock, Tensor, Transform};
+
+#[derive(Debug, Clone, PartialEq, ProcBlock, Default)]
+pub struct ControlLogic {}
+
+impl Transform<(Tensor<u32>, Tensor<u32>)> for ControlLogic {
+    type Output = Tensor<u32>;
+    fn transform(&mut self, inputs: (Tensor<u32>, Tensor<u32>)) -> Self::Output {
+        let mut output: Vec<u32> = Vec::new();
+        let (input1, input2) = inputs;
+        // let input2: Vec<f32> = input2;
+        match input1.elements()[0] {
+            1 => output.push(input2.elements()[0]),
+            _ => output.push(2),
+        }
+
+        Tensor::new_vector(output)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_logic() {
+        let mut logic = ControlLogic::default();
+        let result = logic.transform(([0].into(), [1.0].into()));
+        let should_be: Tensor<u32> = [1].into();
+        assert_eq!(result, should_be)
+    }
+}
